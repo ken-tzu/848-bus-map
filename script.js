04b57a624cb0949ec6e61fd63ed9dd8c834f53e0
Kenneth Krohn <kenneth.krohn@iki.fi>
2024-04-26 15:38:21 +0300

    Read key from config.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var showTraffic = false;
 var map;
 
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    key: "AIzaSyAImnNjxpejsKlQewkCwtBUhyAb560iDSM",
+    key: config.mapsApiKey,
     v: "weekly",
   });
 
@@ -140,7 +140,7 @@ function getData(map) {
                     existingMarker.title = busTitle;
                     existingMarker.position = { lat: val.location.lat, lng: val.location.lng };
                     existingMarker.timeStamp = val.location.timestamp;
-                    existingMarker.content = createIcon();
+                    // existingMarker.content = createIcon();
                     existingMarker.content.style.opacity = val.restState.stopped ? "0.35" : "1.0";
                     existingMarker.content.style.transform = `rotate(${val.location.heading}deg)`;
                     existingMarker.info.setContent(infoContent);
